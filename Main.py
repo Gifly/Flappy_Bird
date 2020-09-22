ee6f4d46258f4fabacfa62d39787dee12a015840
Gifly <A01284296@itesm.mx>
2020-09-22 12:03:52 -0500

    v.1.0.1
    -Assets added
    -Background added
    -Moving floor added
    -Frame rate changed
    -Window size reduced

--- Main.py
+++ Main.py
@@ -1,8 +1,19 @@
 import pygame, sys
 
+def draw_floor():
+    screen.blit(floor_surface, (floor_x_pos,700))
+    screen.blit(floor_surface, (floor_x_pos + 576, 700))
+
 pygame.init()
-screen = pygame.display.set_mode((576,1024)) #Display Surface
+screen = pygame.display.set_mode((576,800)) #Display Surface
 clock = pygame.time.Clock()
+
+bg_surface = pygame.image.load('flappy-bird-assets/sprites/background-day.png').convert()
+bg_surface = pygame.transform.scale2x(bg_surface)
+
+floor_surface = pygame.image.load('flappy-bird-assets/sprites/base.png').convert()
+floor_surface = pygame.transform.scale2x(floor_surface)
+floor_x_pos = 0
 
 #Main Loop
 while True:
@@ -11,7 +22,14 @@ while True:
             pygame.quit()
             sys.exit()
 
+    screen.blit(bg_surface,(0,0))
+    floor_x_pos -= 1
+    draw_floor()
+    if floor_x_pos <= -576:
+        floor_x_pos = 0
+    screen.blit(floor_surface,(floor_x_pos,700))
+
     pygame.display.update()
-    clock.tick(60)
+    clock.tick(120)
 
 pygame.quit()
