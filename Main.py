6f6aa75dda3ef285bc4427106722f65ec0ca4fae
Gifly <A01284296@itesm.mx>
2020-09-23 13:08:51 -0500

    v.1.0.3
    -Collisions added
    -Animations added
    -Score added

--- Main.py
+++ Main.py
@@ -22,14 +22,58 @@ def draw_pipes(pipes):
         else:
             flip_pipe = pygame.transform.flip(pipe_surface,False, True)
             screen.blit(flip_pipe, pipe)
+
+def check_collisions(pipes):
+    for pipe in pipes:
+        if bird_rect.colliderect(pipe):
+            death_sound.play()
+            return False
+        
+    if bird_rect.top <= -100 or bird_rect.bottom >= 700:
+        return False
+
+    return True
+
+def rotate_bird(bird):
+    new_bird = pygame.transform.rotozoom(bird, -bird_movement * 3, 1)
+    return new_bird
+
+def bird_animation():
+    new_bird = bird_frames[bird_index]
+    new_bird_rect= new_bird.get_rect(center = (100, bird_rect.centery))
+    return new_bird,new_bird_rect
+
+def score_display(game_state):
+    if game_state == 'main_game':
+        score_surface = game_font.render(str(int(score)),True, (255,255,255))
+        score_rect = score_surface.get_rect(center = (288, 100))
+        screen.blit(score_surface, score_rect)
+    if game_state == 'game_over':
+        score_surface = game_font.render(f'Score: {int(score)} ',True, (255,255,255))
+        score_rect = score_surface.get_rect(center = (288, 100))
+        screen.blit(score_surface, score_rect)
+
+        high_score_surface = game_font.render(f'High score: {int(high_score)} ',True, (255,255,255))
+        high_score_rect = score_surface.get_rect(center = (288, 600))
+        screen.blit(high_score_surface, high_score_rect)
+
+def update_score(score,high_score):
+    if score > high_score:
+        high_score = score
+    return high_score
+
+pygame.mixer.pre_init(frequency = 44100, size = 16, channels = 1, buffer = 512)
 pygame.init()
 screen = pygame.display.set_mode((576,800)) #Display Surface
 clock = pygame.time.Clock()
+game_font = pygame.font.Font('flappy-bird-assets/04B_19.ttf',40)
 
 # Game Variables
 gravity = 0.25
 bird_movement = 0
-
+game_active = True
+score = 0
+high_score = 0
 
 bg_surface = pygame.image.load('flappy-bird-assets/sprites/background-day.png').convert()
 bg_surface = pygame.transform.scale2x(bg_surface)
@@ -38,9 +82,20 @@ floor_surface = pygame.image.load('flappy-bird-assets/sprites/base.png').convert
 floor_surface = pygame.transform.scale2x(floor_surface)
 floor_x_pos = 0
 
-bird_surface = pygame.image.load('flappy-bird-assets/sprites/bluebird-midflap.png').convert()
-bird_surface = pygame.transform.scale2x(bird_surface)
-bird_rect = bird_surface.get_rect(center = (100,350))
+bird_downflap = pygame.transform.scale2x(pygame.image.load('flappy-bird-assets/sprites/bluebird-downflap.png').convert_alpha())
+bird_midflap = pygame.transform.scale2x(pygame.image.load('flappy-bird-assets/sprites/bluebird-midflap.png').convert_alpha())
+bird_upflap = pygame.transform.scale2x(pygame.image.load('flappy-bird-assets/sprites/bluebird-upflap.png').convert_alpha())
+bird_frames = [bird_downflap, bird_midflap, bird_upflap]
+bird_index = 0
+bird_surface = bird_frames[bird_index]
+bird_rect = bird_surface.get_rect(center = (100, 350))
+
+BIRDFLAP = pygame.USEREVENT + 1 
+pygame.time.set_timer(BIRDFLAP,200)
+
+#bird_surface = pygame.image.load('flappy-bird-assets/sprites/bluebird-midflap.png').convert_alpha()
+#bird_surface = pygame.transform.scale2x(bird_surface)
+#bird_rect = bird_surface.get_rect(center = (100,350))
 
 pipe_surface = pygame.image.load('flappy-bird-assets/sprites/pipe-green.png').convert()
 pipe_surface = pygame.transform.scale2x(pipe_surface)
@@ -49,6 +104,16 @@ SPAWNPIPE = pygame.USEREVENT
 pygame.time.set_timer(SPAWNPIPE, 1200)
 pipe_height = [350,450,600]
 
+game_over_surface = pygame.transform.scale2x(pygame.image.load('flappy-bird-assets/sprites/message.png').convert_alpha())
+game_over_rect = game_over_surface.get_rect(center = (288, 350))
+
+
+#SFX
+flap_sound = pygame.mixer.Sound('flappy-bird-assets/audio/wing.wav')
+death_sound = pygame.mixer.Sound('flappy-bird-assets/audio/hit.wav')
+score_sound = pygame.mixer.Sound('flappy-bird-assets/audio/point.wav')
+score_sound_countdown = 100
+
 #Main Loop
 while True:
     for event in pygame.event.get():
@@ -56,22 +121,53 @@ while True:
             pygame.quit()
             sys.exit()
         if event.type == pygame.KEYDOWN:
-            if event.key ==pygame.K_SPACE:
+            if event.key ==pygame.K_SPACE and game_active:
                 bird_movement = 0
                 bird_movement -= 12
+                flap_sound.play()
+            if event.key ==pygame.K_SPACE and game_active == False:
+                game_active = True
+                pipe_list.clear()
+                bird_rect.center = (100,350)
+                bird_movement = 0
+                score = 0
+
+
         if event.type == SPAWNPIPE:
             pipe_list.extend(create_pipe())
             
+        if event.type == BIRDFLAP:
+            if bird_index < 2:
+                bird_index += 1
+            else:
+                bird_index = 0
+
+            bird_surface, bird_rect = bird_animation()
+
     screen.blit(bg_surface,(0,0))
 
-    #Bird 
-    bird_movement += gravity
-    bird_rect.centery += bird_movement
-    screen.blit(bird_surface,bird_rect)
+    if game_active:
+        #Bird 
+        bird_movement += gravity
+        rotated_bird = rotate_bird( bird_surface)
+        bird_rect.centery += bird_movement
+        screen.blit(rotated_bird,bird_rect)
+        game_active = check_collisions(pipe_list)
 
-    #Pipes
-    pipe_list = move_pipes(pipe_list)
-    draw_pipes(pipe_list)
+        #Pipes
+        pipe_list = move_pipes(pipe_list)
+        draw_pipes(pipe_list)
+
+        score += 0.01
+        score_display('main_game')
+        score_sound_countdown -= 1
+        if score_sound_countdown <= 0:
+            score_sound.play()
+            score_sound_countdown = 100
+    else:
+        screen.blit(game_over_surface,game_over_rect)
+        high_score = update_score(score, high_score)
+        score_display('game_over')
 
     #Floor
     floor_x_pos -= 1
